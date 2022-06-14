a46dc6bc999cfbd7c16e4028da13ad1a27287492
Your Name <you@example.com>
2022-06-15 04:12:08 +0700

    update data

--- app/src/main/java/com/example/teamproject/InputActivity.kt
+++ app/src/main/java/com/example/teamproject/InputActivity.kt
@@ -1,22 +1,82 @@
 package com.example.teamproject
 
+import android.app.DatePickerDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
-import android.widget.EditText
+import android.widget.*
+import com.example.teamproject.databinding.ActivityMainBinding
 import com.example.teamproject.ml.Model
+import java.text.SimpleDateFormat
+import java.util.*
 
 class InputActivity : AppCompatActivity(), View.OnClickListener {
+
+    lateinit var option: Spinner
+    private lateinit var tvDatePicker: TextView
+    private lateinit var btnDatePicker: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_input)
 
+        option = findViewById(R.id.sp_option) as Spinner
+//        result = findViewById(R.id.inputDaerah) as TextView
+        val locations = arrayOf("Bundaran HI","Kelapa Gading","Jagakarsa","Lubang Buaya","Kebon Jeruk")
+
+        option.adapter = ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,locations)
+
+        tvDatePicker = findViewById(R.id.inputTanggal)
+        btnDatePicker = findViewById(R.id.buttonDate)
+
+        val myCalendar = Calendar.getInstance()
+        val datePicker = DatePickerDialog.OnDateSetListener { datePicker, i, i2, i3 ->
+            myCalendar.set(Calendar.YEAR,i)
+            myCalendar.set(Calendar.MONTH,i2)
+            myCalendar.set(Calendar.DAY_OF_MONTH,i3)
+            updateLable(myCalendar)
+        }
+
+        btnDatePicker.setOnClickListener{
+            DatePickerDialog(this,datePicker,myCalendar.get(Calendar.YEAR),myCalendar.get(Calendar.MONTH),
+            myCalendar.get(Calendar.DAY_OF_MONTH)).show()
+        }
+//        option.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+//            override fun onNothingSelected(p0: AdapterView<*>?) {
+//                result.text = "Please select an Option"
+//            }
+//
+//            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+//                result.text = locations.get(p2)
+//            }
+//        }
+
+
+        init()
+
         val btnMoveActivity: Button = findViewById(R.id.button_prediksi)
-        btnMoveActivity.setOnClickListener(this)
+        btnMoveActivity.setOnClickListener{
+            if(tvDatePicker.text.toString().isEmpty()){
+                Toast.makeText(this,"Date input must not be empty",Toast.LENGTH_SHORT).show()
+            }else{
+                val intent = Intent(this, PrediksiAcivity::class.java)
+                startActivity(intent)
+            }
+        }
     }
 
+    private fun updateLable(myCalendar: Calendar) {
+        val myFormat="dd-MM-yyyy"
+        val sdf = SimpleDateFormat(myFormat,Locale.UK)
+        tvDatePicker.setText(sdf.format(myCalendar.time))
+    }
+
+    private fun init(){
+//        result = findViewById(R.id.inputDaerah)
+        tvDatePicker = findViewById(R.id.inputTanggal)
+
+    }
     override fun onClick(p0: View?) {
         when (p0?.id) {
             R.id.button_prediksi -> {
--- app/src/main/java/com/example/teamproject/OutputActivity.kt
+++ app/src/main/java/com/example/teamproject/OutputActivity.kt
@@ -6,11 +6,34 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import java.io.BufferedReader
+import java.io.InputStreamReader
 
 class OutputActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_output)
+
+        val minput = InputStreamReader(assets.open("dki4.csv"))
+        val reader = BufferedReader(minput)
+
+        var line : String?
+        var displayData : String = ""
+        val randoms = (0..450).random().toString()
+        while (reader.readLine().also { line = it } != null){
+            val row : List<String> = line!!.split(",")
+            if (row[0] == randoms) {
+                displayData = displayData + "Polusi pada \t"+row[1] +"\t di Bundaran HI" +"\t sebesar"+
+                        "\n pm10 = \t"+ row[2] + ",\t pm25 = " + row[3]+ ",\t so2 = " + row[4]+ ",\t co = " + row[5]+
+                        ",\t o3 = " + row[6]+ ",\t no2 = " + row[7]+ ", dan Gas Rumah Kaca co2 = " + row[8] +".\n"+
+                        "hasil ISPU ialah Tidak Baik, kami menganjurkan untuk menjaga kesehatan dan menanam pohon jenis ______ sebanyak 3 buah"
+
+            }
+        }
+
+        var txtData = findViewById(R.id.textView7) as TextView
+        txtData.text = displayData
+
         val btnMoveActivity: Button = findViewById(R.id.button_back_home)
         btnMoveActivity.setOnClickListener(this)
 
